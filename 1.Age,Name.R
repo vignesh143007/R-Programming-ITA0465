930bc8d0701bc5dca50d616fd01898639ab3387f
vignesh143007 <137189542+vignesh143007@users.noreply.github.com>
2024-03-20 20:43:55 +0530

    Update 1.Age,Name.R

--- 1.Age,Name.R
+++ 1.Age,Name.R
@@ -1,16 +1,4 @@
-# 17. Create an array with specified dimensions and names for each dimension
-
-# Vector of values
-values <- 1:12
-
-# Vector of dimensions
-dims <- c(3, 2, 2)
-
-# Names for each dimension
-dimnames <- list(c("Dim1_1", "Dim1_2", "Dim1_3"), c("Dim2_1", "Dim2_2"), c("Dim3_1", "Dim3_2"))
-
-# Create array
-array_data <- array(values, dim = dims, dimnames = dimnames)
-
-cat("Array:\n")
-print(array_data)
+name = readline(prompt="Input your name: ")
+age =  readline(prompt="Input your age: ")
+print(paste("My name is",name, "and I am",age ,"years old."))
+print(R.version.string)
